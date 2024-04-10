f0157e46880db35e1f3277ab747fe1b6e43bff40
laboratorio <laboratorio@UPOAULA10304>
2024-04-10 12:08:02 -0500

    Commit 3 Sebastian

---
+++ Migrations/20240410165635_Inicio.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SebastianLargo_TallerMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Burguer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BurgerID = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WithCheese = table.Column<bool>(type: "bit", nullable: false),
+                    Precio = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Salad1 = table.Column<int>(type: "int", nullable: false),
+                    Salad12 = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Burguer", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Burguer");
+        }
+    }
+}
