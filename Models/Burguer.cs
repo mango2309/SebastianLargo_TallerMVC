6a8f9d2506afd567382c068f804f95a26abb75d1
laboratorio <laboratorio@UPOAULA10304>
2024-04-10 11:01:06 -0500

    Agregar archivos de proyecto.

---
+++ Models/Burguer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SebastianLargo_TallerMVC.Models
+{
+    public class Burguer
+    {
+        public int Id { get; set; }
+
+        public int BurgerID { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        [Range(0.01, 9999.99)]
+        public decimal Precio { get; set; }
+
+    }
+}
